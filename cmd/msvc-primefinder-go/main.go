8f28d4a212ba6ca9a7274fe7cb8c0fef762ee3e0
Lewis <lewisthornton@btinternet.com>
2024-12-01 19:36:13 +0000

    basic order routes added and method stubs

--- cmd/msvc-primefinder-go/main.go
+++ cmd/msvc-primefinder-go/main.go
@@ -3,11 +3,11 @@ package main
 import (
 	"context"
 	"fmt"
-	"github.com/LewisT543/msvc-primefinder-go/application"
+	"github.com/LewisT543/msvc-primefinder-go/internal"
 )
 
 func main() {
-	app := application.New()
+	app := internal.New()
 
 	err := app.Start(context.TODO())
 	if err != nil {
